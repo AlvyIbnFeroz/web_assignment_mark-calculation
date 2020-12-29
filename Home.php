cd34608e76e17ccf161437928102ccb173053774
AlvyIbnFeroz <61466349+AlvyIbnFeroz@users.noreply.github.com>
2020-12-29 22:47:24 +0600

    Initial upload still working on it

---
+++ Home.php
@@ -0,0 +1,8 @@
+<strong>Choose an option </strong>
+<form action="Login.php" method="get">
+<input type="submit" value="Login">
+</form>
+
+<form action="Register.php" method="get">
+<input type="submit" value="Register">
+</form>
